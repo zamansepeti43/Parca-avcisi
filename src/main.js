ad99a4b3fc975f96eaf12b4e16236454f8d985a2
AGT Studio <tan.ahmet.at@gmail.com>
2026-08-31 09:50:24 +0300

    feat: activate account section in hamburger menu

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import './lib/listing-creator.js';
 import './lib/photo-limit-ui.js';
 import './lib/account-center.js';
 import './lib/account-menu-fix.js';
+import './lib/account-drawer-ui.js';
 import './lib/saved-vehicles-ui.js';
 import './lib/listing-report.css';
 import './lib/listing-report-ui.js';
